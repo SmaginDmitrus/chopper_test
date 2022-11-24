d371170dfe0d68bba621cf9dc2fab4b099321cc1
SmaginDimitrus <smagindm2002@gmail.com>
2022-11-24 17:08:25 +0300

    Working prototype with flexible system parametrs

--- chopper_test.cpp
+++ chopper_test.cpp
@@ -1,3 +1,4 @@
+
 #include "epot_bicgstabsolver.hpp"
 #include "geometry.hpp"
 #include "func_solid.hpp"
@@ -11,43 +12,48 @@
 #include <string>
 #include <iostream>
 #include <readascii.hpp>
+#include <fstream>
+using namespace std;
 
-double U = 20.0e3; //не больше 30 кВ, а лучше 25кВ
+double U = 25.0e3; //не больше 30 кВ, а лучше 25кВ
 double ground = -40.0e3;
-double  cond_length = 0.15;
-double collector_length = 0.1;
-double cond_width = 0.1;
+double  cond_length;//варьируем от 0.075 до 0.2
+double cond_width;//варьируем от 0.08 до 0.16 м 
 double I0 = 0.0;
 double I1 = 0.0;
 double I2 = 0.0;
+ofstream fresult("results.txt");
+
+
 
 bool cyl_1(double x,double y,double z){
         return (x*x + y*y >= (0.04)*(0.04) && z <= 0.8);
     }
 bool cond_down(double x,double y,double z){
-        return(z>=0.82 && z<= (0.82+ cond_length ) && y>=-0.07 && y<=0.07 && x>=-(cond_width/2.0) && x <= -(cond_width/2.0 - 0.005)); 
+        return(z>=0.81 && z<= (0.81+ cond_length ) && y>=-0.07 && y<=0.07 && x>=-(cond_width/2.0) && x <= -(cond_width/2.0 - 0.005));
     }
 bool cond_up(double x,double y,double z){
-        return(z>=0.82 && z<=(0.82 + cond_length ) && y>=-0.07 && y<=0.07 && x >= (cond_width/2.0)-0.005 && x<=(cond_width/2.0)); 
+        return(z>=0.81 && z<=(0.81 + cond_length ) && y>=-0.07 && y<=0.07 && x >= (cond_width/2.0)-0.005 && x<=(cond_width/2.0));
     }
 bool cyl_2(double x,double y,double z){
-        return (x*x + y*y >= (0.065)*(0.065) && z >= (0.84+ cond_length) && z<=(0.84+ cond_length + collector_length)); 
+        return (x*x + y*y >= (0.065)*(0.065) && z >= (0.82+cond_length) && z<=(1.15));
     }
 bool cyl_3(double x,double y,double z){
-        return (x*x + y*y >= (0.04)*(0.04) && z >= (0.84 + cond_length + collector_length) && z<=1.2); 
+        return (x*x + y*y >= (0.04)*(0.04) && z >= 1.15 && z<=1.2);
     }
 
 
 
 void simu(void){
-
-    double const x = cond_length*1000;
-    double const y = cond_length*1000;
-    double const z = 450;
-    double const mesh_step = 0.002;
-    Geometry geom( MODE_3D, Int3D(ceil(x/(mesh_step*1000)),ceil(y/(mesh_step*1000)),ceil(z/(mesh_step*1000))), Vec3D(-cond_length/2.0,-cond_length/2.0,0.7), mesh_step );
-
     
+
+    double const x = 150;
+    double const y = 150;
+    double const z = 500;
+    double const mesh_step = 0.002;
+    Geometry geom( MODE_3D, Int3D(ceil(x/(mesh_step*1000)),ceil(y/(mesh_step*1000)),ceil(z/(mesh_step*1000))), Vec3D(-0.15/2.0,-0.15/2.0,0.7), mesh_step );
+
+
     Solid *Cyl_1 = new FuncSolid( cyl_1 );
     geom.set_solid( 7, Cyl_1 );
     Solid *Cond_down = new FuncSolid( cond_down );
@@ -77,7 +83,7 @@ void simu(void){
     MeshScalarField scharge( geom );
     MeshVectorField bfield;
     EpotEfield efield( epot );
-    field_extrpl_e efldextrpl[6] = { FIELD_EXTRAPOLATE, FIELD_EXTRAPOLATE, 
+    field_extrpl_e efldextrpl[6] = { FIELD_EXTRAPOLATE, FIELD_EXTRAPOLATE,
                                     FIELD_EXTRAPOLATE,FIELD_EXTRAPOLATE,
                                     FIELD_EXTRAPOLATE, FIELD_EXTRAPOLATE };
     efield.set_extrapolation( efldextrpl );
@@ -87,14 +93,14 @@ void simu(void){
 
     ReadAscii din( "particles", 10 );
     GeomPlotter geomplotter( geom );
-    std::string buffer = "plot";
+    string buffer = "plot";
 
     TrajectoryDiagnosticData tdata;
-    std::vector<trajectory_diagnostic_e> diagnostics;
-    
-  
+    vector<trajectory_diagnostic_e> diagnostics;
 
-    for( int i = 0; i < 5; i++ ) {
+
+
+    for( int i = 0; i < 4; i++ ) {
         solver.solve( epot, scharge );
         efield.recalculate();
         pdb.clear();
@@ -115,37 +121,40 @@ void simu(void){
     }
         buffer = buffer + to_string(i) + ".png";
    pdb.iterate_trajectories( scharge, efield, bfield );
-  
-    geomplotter.set_size( 750, 750 );
-    geomplotter.set_epot( &epot );
-    geomplotter.set_particle_database( &pdb );
-    geomplotter.set_view(VIEW_ZX);
-    geomplotter.plot_png( buffer );
+
+    // geomplotter.set_size( 750, 750 );
+    // geomplotter.set_epot( &epot );
+    // geomplotter.set_particle_database( &pdb );
+    // geomplotter.set_view(VIEW_ZX);
+    // geomplotter.plot_png( buffer );
     }
-    diagnostics.push_back(DIAG_CURR);    
+    diagnostics.push_back(DIAG_CURR);
     pdb.trajectories_at_plane( tdata, AXIS_Z, geom.max(2)-geom.h(), diagnostics );
 
-    for (int i = 0; i < din.rows();i++){
-        if ( i%100 == 0){ std::cout << tdata(0)[i] << std::endl; }
-        I0 += tdata(0)[i];
+    for (size_t i = 0; i < tdata.traj_size();i++){
+            I0 += tdata(0)[i];
     }
-    std::cout << "output current = " << I0 << std::endl;
-
+    cout << "output current = " << I0 << endl;
+    
     pdb.trajectories_at_plane( tdata, AXIS_Z, 0.82, diagnostics );//снимаем ток в начале конденсатора
 
-    for (int i = 0; i < din.rows();i++)
+    for (size_t i = 0; i < tdata.traj_size();i++)
     {
-        if (i % 100 == 0){ std::cout << tdata(0)[i] << std::endl; }
+    
         I1 += tdata(0)[i];
     }
     pdb.trajectories_at_plane( tdata, AXIS_Z, 0.82+cond_length, diagnostics );//снимаем ток через плоскость в конце конденсатора
-    for (int i = 0; i < din.rows();i++)
-    {   
-        if ( i %100 == 0){ std::cout << tdata(0)[i] << std::endl; }
+    for (size_t i = 0; i < tdata.traj_size();i++)
+    {
+    
         I2 += tdata(0)[i];
-    }    
+    }
 
-    std::cout << "cond current = " << I2-I0 << std::endl;
+    cout << "cond current = " << I1-I2<< endl;
+    if ((I2-I1)<0.0005){
+    fresult<<cond_length<<" "<<cond_width<<" "<<I0<<endl;
+    }
+    fresult.close(); 
 }
 
 int main( int argc, char **argv )
@@ -153,7 +162,10 @@ int main( int argc, char **argv )
     try {
 	ibsimu.set_message_threshold( MSG_VERBOSE, 1 );
 	ibsimu.set_thread_count( 4 );
+    for (cond_length = 0.075;cond_length<=0.2;cond_length+=0.001){
+        for(cond_width=0.08;cond_width<=0.16;cond_width+=0.01){
         simu();
+    }}
     } catch ( Error e ) {
         e.print_error_message( ibsimu.message( 0 ) );
         exit( 1 );
@@ -161,3 +173,4 @@ int main( int argc, char **argv )
 
     return( 0 );
 }
+
